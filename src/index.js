ced218ad81e5dabc2439c4bc08407c1b15d30543
babakdorani <babak.dorani@gmail.com>
2021-10-24 01:25:28 +0300

    redux added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
+import registerServiceWorker from "./registerServiceWorker";
+import firebase from "./firebase";
+
 import "semantic-ui-css/semantic.min.css";
 
-const Root = () => {
-  return (
-    <Router>
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
+
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import { setUser } from "./actions";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // console.log(user);
+        this.props.setUser(user);
+        this.props.history.push("/");
+      }
+    });
+  }
+
+  render() {
+    return (
       <Switch>
+        <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <Route path="/" component={App} exact />
       </Switch>
-    </Router>
-  );
-};
+    );
+  }
+}
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+const mapStateFromProps = (state) => ({
+  isLoading: state.user.isLoading,
+});
+
+const RootWithAuth = withRouter(connect(mapStateFromProps, { setUser })(Root));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
