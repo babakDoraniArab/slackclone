05570aab20cec017fab88e635a881e2c85d166f5
babakdorani <babak.dorani@gmail.com>
2021-10-24 11:25:59 +0300

    dropdownOptions done

--- src/components/SidePanel/UserPanel.jsx
+++ src/components/SidePanel/UserPanel.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
-import { Grid, Header, Icon } from "semantic-ui-react";
+import { Dropdown, Grid, Header, Icon } from "semantic-ui-react";
 
 export default class UserPanel extends Component {
+  dropdownOptions = () => [
+    {
+      key: "user",
+      text: (
+        <span>
+          Signed is as <strong>User</strong>{" "}
+        </span>
+      ),
+      disabled: true,
+    },
+    {
+      key: "avatar",
+      text: <span>Change Avatar</span>,
+    },
+    {
+      key: "signout",
+      text: <span>Sign Out</span>,
+    },
+  ];
   render() {
     return (
       <Grid style={{ background: "#4c3c4c" }}>
@@ -12,6 +31,13 @@ export default class UserPanel extends Component {
               <Header.Content>ChatPanel</Header.Content>
             </Header>
           </Grid.Row>
+
+          <Header style={{ padding: "0.25em" }} as="h4" inverted>
+            <Dropdown
+              trigger={<span>User</span>}
+              options={this.dropdownOptions()}
+            />
+          </Header>
         </Grid.Column>
       </Grid>
     );
